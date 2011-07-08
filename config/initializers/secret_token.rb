fb028a4a47e0b95e7f43d5ba18a8ddbe9d3ef3ed
V Sreekanth <sreeix@gmail.com>
2011-07-08 13:34:02 +0530

    Adding rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitFlow::Application.config.secret_token = '3bd1fbf7e0f7a61bf68f868532f34a6363b6892b8851f13988e2406b50108cef2abe9900cd9cd88b88db67b8d1189840c1593baffb26c090f124b238d99ff2dc'
