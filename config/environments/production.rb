0b0e3d05115ca4bb500802d385af65b042745b4f
V Sreekanth <sreeix@gmail.com>
2011-07-09 04:50:20 +0530

    Adding minor tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ BitFlow::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
