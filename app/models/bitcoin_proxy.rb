e409ba1a90d97c80592ca5ce62b348f275ea930f
V Sreekanth <sreeix@gmail.com>
2011-07-16 01:20:22 +0530

    Taking the tests out of the code,  using ruby conventions

--- app/models/bitcoin_proxy.rb
+++ app/models/bitcoin_proxy.rb
@@ -1,68 +1,32 @@
-require 'json'
-require 'rest_client'
-module BitcoinProxy
-    
-    class JSONRPCException < RuntimeError
-      def initialize()
-        super()
-      end
+class BitcoinProxy
+  class JSONRPCException < RuntimeError
+  end
+
+  class ServiceProxy
+    def initialize(service_url)
+      @service_url = service_url
     end
 
-    class ServiceProxy
-        def initialize(service_url)
-            @service_url = service_url
-        end
-    
-        def method_missing(name, *args, &block)
-            postdata = {"method" => name, "params" => args, "id" => "jsonrpc"}.to_json
-            respdata = RestClient.post @service_url, postdata
-            resp = JSON.parse respdata
-            if resp["error"] != nil
-                raise JSONRPCException.new, resp['error']
-            end
-            return resp['result']
-        end
+    def method_missing(name, *args, &block)
+      postdata = {"method" => name, "params" => args, "id" => "jsonrpc"}.to_json
+      respdata = RestClient.post @service_url, postdata
+      resp = JSON.parse respdata
+      raise JSONRPCException.new, resp['error'] if resp["error"]
+      resp['result']
     end
-    
-    def createNewAddressForAccount(accountName)
-        return ServiceProxy.new('http://bitflow:as@127.0.0.1:8332').getnewaddress(accountName)
-    end
-    
-    def getBalance(accountName)
-        return ServiceProxy.new('http://bitflow:as@127.0.0.1:8332').getbalance(accountName)
-    end
-    
-    def getAllAddressesByAccount(accountName)
-        return ServiceProxy.new('http://bitflow:as@127.0.0.1:8332').getaddressesbyaccount(accountName)
-    end
-    
+  end
+
+  def self.new_address(accountName)
+    ServiceProxy.new('http://bitflow:as@127.0.0.1:8332').getnewaddress(accountName)
+  end
+
+  def self.balance(accountName)
+    ServiceProxy.new('http://bitflow:as@127.0.0.1:8332').getbalance(accountName)
+  end
+
+  def self.all_addresses(accountName)
+    ServiceProxy.new('http://bitflow:as@127.0.0.1:8332').getaddressesbyaccount(accountName)
+  end
+
 end
 
-class BitcoinProxyTest
-  include BitcoinProxy
-  
-  def testCreateNewAddressForAccount
-    acc = 'niket'
-    addr = createNewAddressForAccount(acc)
-    puts "#{addr} created for account: #{acc}"
-  end
-  
-  def testGetBalance
-    acc = 'niket'
-    balance = getBalance(acc)
-    puts "Balance for #{acc}: #{balance}"
-  end
-  
-  def testGetAllAddressesByAccount
-    acc = 'niket'
-    addrs = getAllAddressesByAccount(acc)
-    puts "Addresses for #{acc}: #{addrs}"
-  end
-  
-end
-
-test = BitcoinProxyTest.new
-
-#test.testCreateNewAddressForAccount
-test.testGetBalance
-test.testGetAllAddressesByAccount
---
+++ spec/models/bitcoin_proxy_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe BitcoinProxy do
+  it "creates a new accout" do
+    BitcoinProxy.new_address('niket').should_not be_nil
+  end
+
+  it "creates a new account" do
+    BitcoinProxy.all_addresses('niket').should_not be_nil
+  end
+
+  it "get balance" do
+    BitcoinProxy.balance('niket').should_not be_nil
+  end
+  
+end
