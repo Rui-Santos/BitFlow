108c9038868a77fca9906c4511a46faff3e94129
V Sreekanth <sreeix@gmail.com>
2011-08-04 03:22:41 +0530

    Fixing a compile failure

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -1,13 +1,12 @@
 class Commission 
   def self.amount(user)
-  	if user.undiscounted_commission?
-  		commission = Setting.admin.data[:commission_fee]
-  		commission
-  	else
-  		settings = Setting.admin
-  		commission = settings.data[:commission_fee]
-  		discount = settings.data.[:referral_discount_percentage]
-  		commission * ((100 - discount)/100)
-  	end
+    if user.undiscounted_commission?
+      Setting.admin.data[:commission_fee]
+    else
+      settings = Setting.admin
+      commission = settings.data[:commission_fee]
+      discount = settings.data[:referral_discount_percentage]
+      commission * ((100 - discount)/100)
+    end
   end
 end
