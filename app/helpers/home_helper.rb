2e1ae729e8147706fea3b69f9f027f4f0f8c7736
niket kumar <niket@activesphere.com>
2011-08-05 15:49:37 +0530

    a referral code can be used by multiple signups(users)

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,5 @@
 module HomeHelper
-	def unused_referral_code
+	def referral_code
 		if current_user.nil?
 			''
 		else
@@ -7,19 +7,12 @@ module HomeHelper
 		end
 	end
 
-	def unused_referral_code_link
+	def referral_code_link
 		if current_user.nil?
 			''
 		else
-			'Referral Code link: http://localhost:3000/users/sign_up?referrer_code='+current_user.referral_code
+			'Referral Code link: ' + request.url + 'users/sign_up?referrer_code=' + current_user.referral_code
 		end
 	end
 
-	def referral_code_unused?
-		if current_user.nil?
-			false
-		else
-			current_user.referral_code_unused?
-		end
-	end
 end
