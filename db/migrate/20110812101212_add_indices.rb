71bf74cf19eb9315380363a78ab5a7cb0327360b
V Sreekanth <sreeix@gmail.com>
2011-08-12 20:48:18 +0530

    Messing with migrations to reduce confusion

--- db/migrate/20110812101212_add_indices.rb
+++ db/migrate/20110812101212_add_indices.rb
@@ -1,14 +1,5 @@
-class TruncateAllDataButUser < ActiveRecord::Migration
+class AddIndices < ActiveRecord::Migration
   def self.up
-    # Ask.all.each {|x| x.destroy}
-    #     Bankaccount.all.each {|x| x.destroy}
-    #     Bid.all.each {|x| x.destroy}
-    #     BtcWithdrawRequest.all.each {|x| x.destroy}
-    #     FundDepositRequest.all.each {|x| x.destroy}
-    #     FundTransactionDetail.all.each {|x| x.destroy}
-    #     FundWithdrawRequest.all.each {|x| x.destroy}
-    #     Fund.all.each {|x| x.update_attributes :amount=>0.0,:reserved=>0.0,:available=>0.0}
-    #     Trade.all.each {|x| x.destroy}
     UserWallet.all.each {|wallet| wallet.update_attribute :last_received_epoch, 0}
     
     add_index :asks, :status
