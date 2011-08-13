673119f646c4c3db565e01e506a0c3f9fd084d0c
V Sreekanth <sreeix@gmail.com>
2011-08-13 23:14:07 +0530

    Adding the production stage

---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+set :user, "ubuntu"
+
+
+role :web, "ec2-46-137-61-200.eu-west-1.compute.amazonaws.com" # Your HTTP server, Apache/etc
+role :app, "ec2-46-137-61-200.eu-west-1.compute.amazonaws.com" #This may be the same as your `Web` server
+role :db,  "ec2-46-137-61-200.eu-west-1.compute.amazonaws.com", :primary => true # This is where Rails migrations will run
+
