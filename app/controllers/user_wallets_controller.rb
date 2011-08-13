95e3b500ef63fbb44565c3867e1e2586ec83441b
niket kumar <niket@activesphere.com>
2011-08-13 14:46:28 +0530

    improvements around wallet

--- app/controllers/user_wallets_controller.rb
+++ app/controllers/user_wallets_controller.rb
@@ -30,6 +30,8 @@ class UserWalletsController < ApplicationController
         end
       end
       @fund_transaction_details = FundTransactionDetail.order("updated_at desc").where(:user_id => current_user.id, :currency => 'BTC')
+      @btc_withdraw_requests = BtcWithdrawRequest.order("updated_at desc").where(:user_id => current_user.id)
+      @bitcoin_details = BitcoinProxy.list_transactions(current_user.user_wallet.name, 10)
       format.html { render(:action => 'index') }
     end
   end
