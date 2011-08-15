b827a87916e0eec989fd38e264d9bae68f36aa7e
V Sreekanth <sreeix@gmail.com>
2011-08-15 17:12:17 +0530

    Creating different environment/branches for prod/staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :user, "ubuntu"
 
-
+set :branch, 'master'
 role :web, "ec2-46-51-191-212.eu-west-1.compute.amazonaws.com" # Your HTTP server, Apache/etc
 role :app, "ec2-46-51-191-212.eu-west-1.compute.amazonaws.com" #This may be the same as your `Web` server
 role :db,  "ec2-46-51-191-212.eu-west-1.compute.amazonaws.com", :primary => true # This is where Rails migrations will run
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :user, "ubuntu"
 
-
+set :branch, 'develop'
 role :web, "ec2-46-137-131-15.eu-west-1.compute.amazonaws.com"                          # Your HTTP server, Apache/etc
 role :app, "ec2-46-137-131-15.eu-west-1.compute.amazonaws.com"                          # This may be the same as your `Web` server
 role :db,  "ec2-46-137-131-15.eu-west-1.compute.amazonaws.com", :primary => true # This is where Rails migrations will run
