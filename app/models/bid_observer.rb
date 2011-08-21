98d3c93260388d6f5dff0a3265ff0703a01385ec
V Sreekanth <sreeix@gmail.com>
2011-08-21 23:06:45 +0530

    Replacing the bid matching with the methods in user.

--- app/models/bid_observer.rb
+++ app/models/bid_observer.rb
@@ -10,52 +10,32 @@ class BidObserver < ActiveRecord::Observer
     bid_amount_remaining = bid.amount_remaining
     bid.match.each do |ask|
       break if bid_amount_remaining == 0
-      traded_price = 0.0
-      traded_amount = 0.0
-      if bid_amount_remaining >= ask.amount_remaining
-        traded_price = ask.price
-        traded_amount = ask.amount_remaining
-      else
-        traded_price = ask.price
-        traded_amount = bid_amount_remaining
-      end
+      traded_price = ask.price
+      traded_amount =  bid_amount_remaining >= ask.amount_remaining ? ask.amount_remaining : bid_amount_remaining
+
       trade = Trade.create(ask: ask, bid: bid, market_price: traded_price, amount: traded_amount, status: Trade::Status::CREATED)
-      buyer_usd_fund.unreserve!(bid.order_price * traded_amount)
-      buyer_usd_fund.debit! :amount => (traded_price * traded_amount),
-                            :tx_code => FundTransactionDetail::TransactionCode::BITCOIN_PURCHASED,
-                            :currency => 'USD',
-                            :status => FundTransactionDetail::Status::PENDING,
-                            :user_id => bid.user.id,
-                            :trade_id => trade.id,
-                            :ask_id => ask.id,
-                            :bid_id => bid.id
-      buyer_btc_fund.credit! :amount => traded_amount,
-                            :tx_code => FundTransactionDetail::TransactionCode::BITCOIN_PURCHASED,
-                            :currency => 'BTC',
-                            :status => FundTransactionDetail::Status::PENDING,
-                            :user_id => bid.user.id,
-                            :trade_id => trade.id,
-                            :ask_id => ask.id,
-                            :bid_id => bid.id
-      seller_usd_fund = ask.user.usd
-      seller_btc_fund = ask.user.btc
-      seller_btc_fund.unreserve!(traded_amount)
-      seller_usd_fund.credit! :amount => (traded_price * traded_amount),
-                              :tx_code => FundTransactionDetail::TransactionCode::BITCOIN_SOLD,
-                              :currency => 'USD',
-                              :status => FundTransactionDetail::Status::PENDING,
-                              :user_id => ask.user.id,
-                              :trade_id => trade.id,
-                              :ask_id => ask.id,
-                              :bid_id => bid.id
-      seller_btc_fund.debit! :amount => traded_amount,
-                              :tx_code => FundTransactionDetail::TransactionCode::BITCOIN_SOLD,
-                              :currency => 'BTC',
-                              :status => FundTransactionDetail::Status::PENDING,
-                              :user_id => ask.user.id,
-                              :trade_id => trade.id,
-                              :ask_id => ask.id,
-                              :bid_id => bid.id
+      bid.user.buy_btc(traded_price, traded_amount, trade)
+      #Something not right here.
+      # takl to niket
+      # buyer_usd_fund.unreserve!(bid.order_price * traded_amount)
+      #       buyer_usd_fund.debit! :amount => (traded_price * traded_amount),
+      #                             :tx_code => FundTransactionDetail::TransactionCode::BITCOIN_PURCHASED,
+      #                             :currency => 'USD',
+      #                             :status => FundTransactionDetail::Status::PENDING,
+      #                             :user_id => bid.user.id,
+      #                             :trade_id => trade.id,
+      #                             :ask_id => ask.id,
+      #                             :bid_id => bid.id
+      #       buyer_btc_fund.credit! :amount => traded_amount,
+      #                             :tx_code => FundTransactionDetail::TransactionCode::BITCOIN_PURCHASED,
+      #                             :currency => 'BTC',
+      #                             :status => FundTransactionDetail::Status::PENDING,
+      #                             :user_id => bid.user.id,
+      #                             :trade_id => trade.id,
+      #                             :ask_id => ask.id,
+      #                             :bid_id => bid.id
+      ask.user.sell_btc(traded_price, traded_amount, trade)
+      
       bid_amount_remaining = bid_amount_remaining - traded_amount
       ask_amount_remaining = ask.amount_remaining - traded_amount
       ask.update_attribute(:amount_remaining, ask_amount_remaining)
